68ca336663e48defe78254b932fa9daf4acc2d1b
Visa Pollari <dev@visapollari.fi>
2026-08-15 15:05:17 +0300

    categories

--- wikigraph/wiki.py
+++ wikigraph/wiki.py
@@ -1,37 +1,28 @@
 import wikipediaapi
-import json
 import asyncio
 
-# Asynchronous client
-wiki = wikipediaapi.AsyncWikipedia(user_agent='6 degrees of separation - game (gamejam@visapollari.fi)', language='en')
-
-# save.json is a JSON array of objects, where there are people from wikipedia (just names) and as a child a list a people who they are connected to in wikipedia, and check if player has defeated that person or not.
-def save_to_graph(graph):
-    pass
+wiki = wikipediaapi.AsyncWikipedia(
+    user_agent="6 degrees of separation - game (gamejam@visapollari.fi)",
+    language="en"
+)
 
 
 async def find_random_person():
-    person = None
-    while person is None:
-        # Get a random page from Wikipedia
-        page = await wiki.random()
-        print_categories(page)
-        print("Random page title:", page)
-        full_page = wiki.page(page)
-        print("full_page:", full_page)
-        category = await full_page.categories
-        print("category:", category)
-    return person
+    while True:
+        random_pages = await wiki.random(limit=1)
 
-def find_connected_people():
-    pass
-    
+        title, page = next(iter(random_pages.items()))
 
-async def print_categories(page):
-    categories = await page.categories
-    for title in sorted(categories.keys()):
-        print("%s: %s" % (title, categories[title]))
+        print("Random page title:", title)
 
+        categories = await page.categories
+
+        print("Categories:")
+        for category in categories:
+            print(" ", category)
+
+        if "Category:Living people" in categories:
+            return title
 
 
 if __name__ == "__main__":
